84eecd81397e288e55a6781396ebadd0b56870c0
ppk <72378463+ipalakchopra@users.noreply.github.com>
2025-05-23 01:01:56 +0530

    pdf querying

--- app.py
+++ app.py
@@ -1,3 +1,52 @@
 # DOCUMENTATION QUERYING APPLICATION
 
 #Importing Libraries
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+from dotenv import load_dotenv
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+import time
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+#Load Environment Variables
+load_dotenv()
+
+#Langsmith Tracing
+os.environ["LANGCHAIN_HANDLER"] = "langsmith"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGSMITH_PROJECT"] = "Doc-Querying"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+#client = Client()
+start = time.process_time()
+
+# Mistral LLM
+llm = OllamaLLM(model="mistral:7b")
+
+#Prompt Template
+prompt = ChatPromptTemplate.from_template("""
+Answer the following question based only on the provided context.
+Think step by step before providing a detailed answer.
+<context>
+{context}
+</context>
+Question: {input}""")
+
+#Vector DB
+vectordb = Chroma(persist_directory="./chromadb",embedding_function=HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
+
+#Retriever
+retriever = vectordb.as_retriever()
+
+#Chains
+document_chain = create_stuff_documents_chain(llm, prompt)
+retrieval_chain = create_retrieval_chain(retriever,document_chain)
+print(retrieval_chain.invoke({'input':"How to create new topics in Kafka"})['answer'])
+print("Response time:", time.process_time()-start)
+
+#print(llm.invoke("What is kafka"))
--- embedding.py
+++ embedding.py
@@ -11,13 +11,12 @@ loader = PyPDFLoader("Data/kafka.pdf")
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
 documents=text_splitter.split_documents(docs)
-print(documents[:5])
+print(documents)
 
 #Vector Embeddings
-db = Chroma.from_documents(persist_directory="./chromadb", documents=documents[:20],embedding=HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
+db = Chroma.from_documents(persist_directory="./chromadb", documents=documents,embedding=HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
 
 vectordb = Chroma(persist_directory="./chromadb",embedding_function=HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
 print(db.search("kafka","similarity")[0].page_content)
 print()
 print(vectordb.search("kafka","similarity")[0].page_content)
-
